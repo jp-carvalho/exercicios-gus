a779f29de2e1f89d2a18987a40969859e44de8d3
joao-paulo-carvalho <desenhojp@gmail.com>
2022-12-10 12:10:44 -0300

    exercicio 03

--- 03-pagamentoDeLoja.js
+++ 03-pagamentoDeLoja.js
@@ -7,6 +7,12 @@
 // Ou 3x de: R$20,00
 // =========================
 
-function payments(price) {
-    
-}
+let preco = 60;
+let aVista = 10;
+
+totalAVista = preco - ((aVista / 100) * preco);
+
+console.log(`Preço: R$${preco},00`);
+console.log(`À vista: R$${totalAVista},00`);
+console.log(`Ou 3x de: R$${preco / 3},00`);
+
